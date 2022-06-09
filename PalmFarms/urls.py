44b3d1e924e84738465ba25bc960a763a15448a8
Fawaz <abdulsalamfawaz4@gmail.com>
2022-06-09 23:27:52 +0100

    added about page

--- PalmFarms/urls.py
+++ PalmFarms/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from PalmFarms.views import LandingPage
+from PalmFarms.views import LandingPage, AboutPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPage.as_view(), name='landing-page'),
+    path('about', AboutPage.as_view(), name='about'),
     path('', include(('products.urls', 'products'))),
     path('accounts/', include(('accounts.urls', 'accounts'))),
     path('chat/', include(('chat.urls', 'chat'))),
--- PalmFarms/views.py
+++ PalmFarms/views.py
@@ -4,3 +4,7 @@ from accounts.mixins import UnAuthenticatedUserMixin
 
 class LandingPage(UnAuthenticatedUserMixin, TemplateView):
     template_name = "landing-page.html"
+
+
+class AboutPage(TemplateView):
+    template_name = "about-page.html"
